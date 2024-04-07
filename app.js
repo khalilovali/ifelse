510eccd8ad49671129149e989f7236641df32dda
unknown <xaliloff209@gmail.com>
2024-04-07 15:40:28 +0400

    ifelseswitch task is done

---
+++ app.js
@@ -0,0 +1,116 @@
+//const value = 63;
+//const age = 34;
+//if (age >= value) {
+//  console.log("You can retire");
+//} else if (age < value) {
+// console.log("Can not retire");
+//}
+
+//Second Question
+//const num = 4;
+//if (num % 2 == 0) {
+//  console.log("The number is even.");
+//} else {
+//console.log("The number is odd.");
+//}
+
+//3rd question
+//const value = july;
+//const season = summer;
+//if ((season = value)) {
+//console.log("İt is summer");
+//}
+
+//4th question
+//const value = 14;
+//const num1 = 3;
+//const num2 = 5;
+//if (value % num1 == 0 && value % num2 == 0) {
+// console.log("yes");
+//} else {
+//console.log("No");
+//}
+
+//5th question
+//const value = 95;
+//const num = 2;
+//if (value % num == 0) {
+//console.log("Divided");
+//} else {
+//console.log(value / num);}
+
+//6th question
+//const num1 = 3;
+//const num2 = -5;
+//if (num1 * num2 > 0) {
+//console.log("The number is positive");
+//} else {
+//console.log("The number is negative");}
+
+//7th question
+//const num = 405;
+//if (num > 1 && num < 100) {
+//console.log("It is correct");}
+//else{
+//console.log("Incorrect");}
+
+//8th question
+//const num1 = 234;
+//const num2 = 657;
+//const num3 = 129;
+
+//if (num1 >= num2 && num1 >= num3) {
+//largest = num1;
+//} else if (num2 >= num1 && num2 >= num3) {
+//largest = num2;
+//} else {
+//largest = num3;}
+
+//console.log("The largest number is " + largest);
+
+//9th question
+//const num1 = 55;
+
+//if (num1 >= 90) {
+//console.log("A");
+//}
+//if (num1 >= 80 && num < 90) {
+//console.log("B");
+//}
+//if (num1 >= 70 && num1 < 80) {
+//console.log("C");
+//}
+//if (num1 >= 60 && num1 < 70) {
+//console.log("D");
+//}
+//if (num1 < 60) {
+//console.log("F");
+//}
+
+//11th question
+//const age = 44;
+//if (age <= 18) {
+//console.log("Teenager");
+//}
+
+//if (age > 18 && age < 64) {
+//console.log("Adult");
+//}
+//if (age > 64) {
+//console.log("Old");
+//}
+
+//12th question
+//const x = 1;
+//const y = 0;
+//if (x > 0 && y < 0) {
+//console.log(4 * x + 2 * y + 4);
+//}
+//if (x > 0 && y == 0) {
+//console.log(2 * x - y + 3);
+//}
+//if (x < 0 && y > 0) {
+// console.log(3 * x + 4 * y + 3);}
+
+//13th question
+const num = 53;
