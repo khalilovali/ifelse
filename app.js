eab30b7f23ba719c4c0a71f0b3c82d2bdaa19458
unknown <xaliloff209@gmail.com>
2024-07-12 22:21:05 +0400

    task is done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // if (age >= value) {
 //  console.log("You can retire");
 // } else  {
-//   console.log("Can not retire");
+//   document.right("Can not retire");
 // }
 
 //Second Question
@@ -15,37 +15,37 @@
 //}
 
 //3rd question
-const month = "February";
+//const month = "February";
 //! Version 1
-const winterMonths = ["December", "January", "February"];
-const springMonths = ["March", "April", "May"];
-const summerMonths = ["June", "July", "August"];
+// const winterMonths = ["December", "January", "February"];
+// const springMonths = ["March", "April", "May"];
+// const summerMonths = ["June", "July", "August"];
 
-if (winterMonths?.includes(month)) {
-  console.log("It is winter");
-} else if (springMonths?.includes(month)) {
-  console.log("It is spring");
-} else if (summerMonths?.includes(month)) {
-  console.log("It is summer");
-} else {
-  console.log("It is autumn");
-}
+// if (winterMonths?.includes(month)) {
+//   console.log("It is winter");
+// } else if (springMonths?.includes(month)) {
+//   console.log("It is spring");
+// } else if (summerMonths?.includes(month)) {
+//   console.log("It is summer");
+// } else {
+//   console.log("It is autumn");
+// }
 
-if (month === "December" || month === "January" || month === "February") {
-  console.log("It is winter");
-} else if (month === "March" || month === "April" || month === "May") {
-  console.log("It is spring");
-} else if (month === "June" || month === "July" || month === "August") {
-  console.log("It is summer");
-} else if (
-  month === "September" ||
-  month === "October" ||
-  month === "November"
-) {
-  console.log("It is autumn");
-} else {
-  console.log("Invalid month");
-}
+// if (month === "December" || month === "January" || month === "February") {
+//   console.log("It is winter");
+// } else if (month === "March" || month === "April" || month === "May") {
+//   console.log("It is spring");
+// } else if (month === "June" || month === "July" || month === "August") {
+//   console.log("It is summer");
+// } else if (
+//   month === "September" ||
+//   month === "October" ||
+//   month === "November"
+// ) {
+//   console.log("It is autumn");
+// } else {
+//   console.log("Invalid month");
+// }
 
 //4th question
 //const value = 14;
@@ -118,43 +118,85 @@ if (month === "December" || month === "January" || month === "February") {
 // }
 
 //11th question
-const age = 44;
-if (age <= 18) {
-  console.log("Teenager");
-} else if (age > 18 && age < 64) {
-  console.log("Adult");
-} else if (age > 64) {
-  console.log("Old");
-} else {
-  console.log("invalid age");
-}
+// const age = 44;
+// if (age <= 18) {
+//   console.log("Teenager");
+// } else if (age > 18 && age < 64) {
+//   console.log("Adult");
+// } else if (age > 64) {
+//   console.log("Old");
+// } else {
+//   console.log("invalid age");
+// }
 
-//12th question
-const x = 1;
-const y = 0;
-if (x > 0 && y < 0) {
-  console.log(4 * x + 2 * y + 4);
-} else if (x > 0 && y == 0) {
-  console.log(2 * x - y + 3);
-} else if (x < 0 && y > 0) {
-  console.log(3 * x + 4 * y + 3);
-}
+// //12th question
+// const x = 1;
+// const y = 0;
+// if (x > 0 && y < 0) {
+//   console.log(4 * x + 2 * y + 4);
+// } else if (x > 0 && y == 0) {
+//   console.log(2 * x - y + 3);
+// } else if (x < 0 && y > 0) {
+//   console.log(3 * x + 4 * y + 3);
+// }
 
 //13th question
-const defaultNum = 45;
-const num = 35;
+// const defaultNum = 45;
+// const num = prompt("please enter number");
 
-const lastNumber = defaultNum % 10;
-const firstNumber = parseInt(defaultNum / 10);
-const result = lastNumber * 10 + firstNumber;
+// const lastNumber = defaultNum % 10;
+// const firstNumber = parseInt(defaultNum / 10);
+// const result = lastNumber * 10 + firstNumber;
 
-const lastNumberOfNum = num % 10;
-const firstNumberOfNum = parseInt(num / 10);
+// const lastNumberOfNum = num % 10;
+// const firstNumberOfNum = parseInt(num / 10);
 
-if (defaultNum === num) {
-  console.log("You win 1kazn");
-} else if (num == result) {
-  console.log("You win 500azn");
-} else if (lastNumberOfNum === lastNumber || firstNumberOfNum === firstNumber) {
-  console.log("You win 100azn");
-} else console.log("Try again!!!");
+// if (defaultNum === +num) {
+//   document.write("You win 1kazn");
+// } else if (num == result) {
+//   document.write("You win 500azn");
+// } else if (lastNumberOfNum === lastNumber || firstNumberOfNum === firstNumber) {
+//   document.write("You win 100azn");
+// } else document.write("Try again!!!");
+
+// let a = "5";
+// let b = 10;
+// console.log(a + b); //510
+
+// let d = "5";
+// let c = "10";
+// console.log(a + b); //510
+
+// let e = "5";
+// let f = "10";
+// console.log(e - f); //-5
+
+// let g = "5";
+// let h = "salam";
+// console.log(g + h); //5salam
+// console.log(g - h); //NaN
+
+//NaN type =(number)
+
+//typeof null  is object
+
+//Boolean([]) true
+// String({}); [object,object]
+// Number(null)   0
+
+// let x = 220;
+// let y = "Hello";
+// let z = undefined;
+
+//console.log(x || z); 220;
+//x && y;  y
+
+// for (let i=0; i<=Array.length, i++){
+//   console.log(arr[i]);
+// }
+
+// const [1,2, ...rest] =
+
+// let num;
+// num = 5 + 5;
+// console.log(num);
